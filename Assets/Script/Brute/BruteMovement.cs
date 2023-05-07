20e71aea20f01205488c4253d65587f18d1acfc1
Panziera Ivan <113657486+PanzieraIvan@users.noreply.github.com>
2023-05-07 06:36:14 -0300

    Primera Entraga
    Primera entrega del proyecto final

---
+++ Assets/Script/Brute/BruteMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruteMovement : MonoBehaviour
+{
+    [SerializeField] private float speedBrute = 10f;
+    [SerializeField] private float rotationSpeedBrute = 30f;
+    public Animator m_bruteAnimation;
+
+    public void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        var l_moventDir = new Vector3(moveHorizontal, 0, moveVertical);
+        l_moventDir.Normalize();
+
+        transform.position += l_moventDir * speedBrute * Time.deltaTime;
+        if (l_moventDir != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(l_moventDir), rotationSpeedBrute * Time.deltaTime);
+
+        m_bruteAnimation.SetFloat("SpeedBrute", l_moventDir.magnitude * speedBrute);
+    }
+    void Update()
+    {
+        MovePlayer();
+    }
+}
