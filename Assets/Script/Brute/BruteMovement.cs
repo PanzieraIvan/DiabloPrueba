325fc6c24cb389a231f794b98b3caa549659b724
Panziera Ivan <113657486+PanzieraIvan@users.noreply.github.com>
2023-05-12 23:44:43 -0300

    Revert "cambie animacion" (#4)

--- Assets/Script/Brute/BruteMovement.cs
+++ Assets/Script/Brute/BruteMovement.cs
@@ -8,75 +8,27 @@ public class BruteMovement : MonoBehaviour
     [SerializeField] private float rotationSpeedBrute = 30f;
     public Animator m_bruteAnimation;
     private bool m_atackPlayer;
-    private bool canMOve;
-    private bool canAtack;
-
-
-    private void Start()
-    {
-        m_atackPlayer = false;
-        canAtack = false;
-    }
-
 
     public void MovePlayer()
     {
-      
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        var l_moventDir = new Vector3(moveHorizontal, 0, moveVertical);
+        l_moventDir.Normalize();
 
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-            var l_moventDir = new Vector3(moveHorizontal, 0, moveVertical);
-            l_moventDir.Normalize();
+        transform.position += l_moventDir * speedBrute * Time.deltaTime;
+        if (l_moventDir != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(l_moventDir), rotationSpeedBrute * Time.deltaTime);
 
-            transform.position += l_moventDir * speedBrute * Time.deltaTime;
-            if (l_moventDir != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(l_moventDir), rotationSpeedBrute * Time.deltaTime);
+        m_bruteAnimation.SetFloat("SpeedBrute", l_moventDir.magnitude * speedBrute);
 
-            m_bruteAnimation.SetFloat("SpeedBrute", moveHorizontal/*l_moventDir.magnitude * speedBrute*/);
-            m_bruteAnimation.SetFloat("SpeedBrute1", moveVertical/*l_moventDir.magnitude * speedBrute*/);
-
-         
-    }
-
-
-    public void Aatk()
-    {
-        
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetMouseButtonDown(0))
         {
-            
-            m_bruteAnimation.SetTrigger("Atackk");
-
-            //m_atackPlayer = !m_atackPlayer;
-            //m_br
-            //uteAnimation.SetBool("Atack", m_atackPlayer);
-            Debug.Log("aaaaaa");
-            speedBrute = 0f;
-            
+            m_atackPlayer = !m_atackPlayer;
+            m_bruteAnimation.SetBool("Atack", m_atackPlayer);
         }
-       
     }
-
-
     void Update()
-    { 
-       
-            MovePlayer();
-        
-        
-            Aatk();
-        
-
-  
-
+    {
+        MovePlayer();
     }
-    //public void Ataca()
-    //{
-    //    canMOve = false;
-    //}
-    //public void DejaDeAtacar()
-    //{
-    //    canMOve = true;
-    //}
-
 }
